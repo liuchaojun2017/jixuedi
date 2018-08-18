3279043eb39f0648bfeb89635912ff106656a7f9
liuchaojun <15538395097@163.com>
2018-08-18 17:10:24 +0800

    jixuedi的ssm框架搭建1

---
+++ src/main/java/cn/jpp/dao/IUserInfoDAO.java
@@ -0,0 +1,10 @@
+package cn.jpp.dao;
+
+import cn.jpp.entity.UserInfo;
+
+/**
+ * Created by 景佩佩 on 2017/1/16.
+ */
+public interface IUserInfoDAO {
+    public void add(UserInfo info);
+}
---
+++ src/main/java/cn/jpp/service/IUserInfoService.java
@@ -0,0 +1,10 @@
+package cn.jpp.service;
+
+import cn.jpp.entity.UserInfo;
+
+/**
+ * Created by 景佩佩 on 2017/1/16.
+ */
+public interface IUserInfoService {
+    public void add(UserInfo info);
+}
